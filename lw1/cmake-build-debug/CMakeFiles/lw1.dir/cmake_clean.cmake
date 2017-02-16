d99ccb5e6afc7d6ea638a21a1cf39949265c0a33
lulldev <lulldev@yandex.ru>
2017-02-16 19:11:45 +0300

    init

---
+++ lw1/cmake-build-debug/CMakeFiles/lw1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lw1.dir/main.cpp.o"
+  "lw1.pdb"
+  "lw1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lw1/cmake-build-debug/CMakeFiles/lw1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lw1.
+# This may be replaced when dependencies are built.
