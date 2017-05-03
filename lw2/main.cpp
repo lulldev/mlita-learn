8462d0f4d67c727e7f0cb11357d31aa3b31687d6
lulldev <lulldev@yandex.ru>
2017-05-03 11:18:29 +0300

    создан каркас для лаб. работы №2 + catch тесты

---
+++ lw2/main.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include "TriangleSum/TriangleSum.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Program arguments startup error\n" << "Usage: program <input file> <output file>\n";
+        return 1;
+    }
+
+    ifstream inputFile(argv[1]);
+    ofstream outputFile(argv[2]);
+
+    if (!inputFile.is_open() || !outputFile.is_open())
+    {
+        cout << "Error i/o files: " << argv[1] << endl;
+        return 1;
+    }
+
+    if (inputFile.fail() || !outputFile.is_open())
+    {
+        cout << "Fail i/o files" << endl;
+        return 1;
+    }
+
+    string fileLine;
+    getline(inputFile, fileLine);
+//    int lenght;
+//
+//    try
+//    {
+//        lenght = stoi(fileLine);
+//    }
+//    catch(...)
+//    {
+//        cout << "Error converting number" << endl;
+//        return 1;
+//    }
+
+    return 0;
+}
---
+++ lw2/testing.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <iostream>
+
+#include "lib/catch.hpp"
+#include "TriangleSum/TriangleSum.h"
+
+using namespace std;
+
+TEST_CASE("IsBracketsValid with invalid brackets", "[IsBracketsValid]") {
+//    REQUIRE(IsBracketsValid(")") == false);
+}
