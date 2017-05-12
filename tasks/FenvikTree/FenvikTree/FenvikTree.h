43ba3108b38945641f9e97bfe9f764c0d391c562
lulldev <lulldev@yandex.ru>
2017-05-12 10:57:54 +0300

    готовая консольная реализация

--- tasks/FenvikTree/FenvikTree/FenvikTree.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-
-void init (int nn);
-int sum (int r);
-void inc (int i, int delta);
-int sum (int l, int r);
-void init (std::vector<int> a);
--- tasks/FenvikTree/main.cpp
+++ tasks/FenvikTree/main.cpp
@@ -3,9 +3,57 @@
 #include <fstream>
 
 #include "FenvikTree/Helpers.h"
-#include "FenvikTree/FenvikTree.h"
 
 using namespace std;
+
+std::vector<int> t;
+int n;
+
+void init (int nn)
+{
+    n = nn;
+    t.assign (n, 0);
+}
+
+void add (int i, int delta)
+{
+    for (; i < n; i = (i | (i+1)))
+    {
+        t[i] += delta;
+    }
+}
+
+void init (vector<int> a)
+{
+    init ((int) a.size());
+    for (unsigned i = 0; i < a.size(); i++)
+    {
+        add (i, a[i]);
+    }
+}
+
+int sum (int r)
+{
+    int result = 0;
+    for (; r >= 0; r = (r & (r+1)) - 1)
+    {
+        result += t[r];
+    }
+    return result;
+}
+
+int sum (int l, int r)
+{
+    return sum (r) - sum (l-1);
+}
+
+void ShowTree()
+{
+    for (int i = 0; i < t.size(); i++)
+    {
+        cout << i << ":" << t[i] << endl;
+    }
+}
 
 int main(int argc, const char * argv[])
 {
@@ -49,8 +97,58 @@ int main(int argc, const char * argv[])
     }
 
     init(arrayElements);
-    
 
-    cout << arrayElements[1] << endl;
+    cout << "Builded tree ready:" << endl;
+    ShowTree();
+
+    string inputMenuItem;
+    string command;
+
+    cout << "Fenvik commands:\n"
+         << "1 - Add (i, d)\n"
+         << "2 - Rsq (i, j)\n"
+         << "3 - Show tree\n"
+         << "Input your command >>";
+
+    while(cin >> inputMenuItem)
+    {
+        if (isdigit(inputMenuItem[0]))
+        {
+            if (inputMenuItem == "1")
+            {
+                int i, d;
+                cout << "Input i = ";
+                cin >> i;
+                cout << "Input d = ";
+                cin >> d;
+                add(i, d);
+                ShowTree();
+            }
+            else if (inputMenuItem == "2")
+            {
+                int i, j;
+                cout << "Input i = ";
+                cin >> i;
+                cout << "Input j = ";
+                cin >> j;
+                cout << sum (i, j) << endl;
+            }
+            else if (inputMenuItem == "3")
+            {
+                ShowTree();
+            }
+            else
+            {
+                cout << "Unknow command. Input please [1-3]." << endl;
+            }
+        }
+        else
+        {
+            cout << "Input number of command [1-3]" << endl;
+        }
+        cout << "Input your command [1-3] >>";
+    }
+
+
     return 0;
 }
