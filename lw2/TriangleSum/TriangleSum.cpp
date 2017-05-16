30923efd35bb7415342031c60a2c2df50d57a72f
lulldev <lulldev@yandex.ru>
2017-05-16 20:51:10 +0300

    реализован цикл нахождения пары детей у родителя

--- lw2/TriangleSum/TriangleSum.cpp
+++ lw2/TriangleSum/TriangleSum.cpp
@@ -31,59 +31,38 @@ const vector<int> SplitIntegers(const string& targetString, const char& delimite
 void CalculateTriangleMaxSum(int triangleWeight, std::vector<std::vector<int>>& vectorOfTriangleData,
                              std::ofstream& outputFile)
 {
-    int triangleLevelCounter = triangleWeight - 1;
-    std::map <string, string> resultWayAsMaxSum; // example {"way": "sum"}
-    while (triangleLevelCounter >= 0)
+    //int triangleLevelCounter = triangleWeight - 1;
+    //std::map <string, string> resultWayAsMaxSum; // example {"way": "sum"}
+
+    std::vector<std::vector<int>> vectorOfRelativeSums;
+    triangleWeight = triangleWeight - 1;
+
+    bool isProcessed = false;
+    while (!isProcessed)
     {
-        int lineElement = 0;
-        while (lineElement != vectorOfTriangleData[triangleLevelCounter].size())
+        int currentLineCounter = triangleWeight - 1;
+        while (currentLineCounter >= 0) // цикл по строке m - n
         {
-            int currentLineElement = vectorOfTriangleData[triangleLevelCounter][lineElement];
-            if (lineElement == 0)
+            int currentPosition = 0;
+            while (currentPosition <= currentLineCounter)
             {
-                int lineCounter = lineElement; // == 0
-                int sum = 0;
-                string way("");
-                while (lineCounter != (triangleWeight))
-                {
-                    sum += vectorOfTriangleData[lineCounter][0];
-                    way += to_string(vectorOfTriangleData[lineCounter][0]);
-                    lineCounter++;
-                }
-                resultWayAsMaxSum.insert(pair<string, string>(way, to_string(sum)));
+                cout << vectorOfTriangleData[currentLineCounter][currentPosition] << endl;
+                cout << "Daughters:" << endl;
+                cout << vectorOfTriangleData[currentLineCounter + 1][currentPosition] << endl;
+                cout << vectorOfTriangleData[currentLineCounter + 1][currentPosition + 1] << endl;
+                cout << endl;
+                currentPosition++;
             }
-            else if (lineElement == vectorOfTriangleData[triangleLevelCounter].size() - 1)
-            {
-                int lineCounter = 0; // == last element in line
-                int sum = 0;
-                string way("");
-                while (lineCounter != (triangleWeight))
-                {
-                    int lastLineElement = vectorOfTriangleData[lineCounter].size() - 1;
-                    sum += vectorOfTriangleData[lineCounter][lastLineElement];
-                    way += to_string(vectorOfTriangleData[lineCounter][lastLineElement]);
-                    lineCounter++;
-                }
-                resultWayAsMaxSum.insert(pair<string, string>(way, to_string(sum)));
-            }
-            else
-            {
-                int upCounter = 0;
-                while (lineCounter != (triangleWeight))
-                {
-
-                }
-            }
-            lineElement++;
+            currentLineCounter--;
         }
-        triangleLevelCounter--;
+
+        isProcessed = true;
     }
 
-
-    for (auto itResultMap = resultWayAsMaxSum.begin(); itResultMap != resultWayAsMaxSum.end(); ++itResultMap)
-    {
-        cout << itResultMap->first << " : " << itResultMap->second << "\t\t   " << endl;
-    }
+//    for (auto itResultMap = resultWayAsMaxSum.begin(); itResultMap != resultWayAsMaxSum.end(); ++itResultMap)
+//    {
+//        cout << itResultMap->first << " : " << itResultMap->second << "\t\t   " << endl;
+//    }
 
 
 }
