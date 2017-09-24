844c9fe4f32b56447a17c1e5d69c6924fcea1933
lulldev <lulldev@yandex.ru>
2017-09-24 15:45:30 +0300

    ready split function and accumulate vector

--- other/switch-sign/main.cpp
+++ other/switch-sign/main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <numeric>
 
 #define INPUT_FILENAME "input.txt"
 #define OUTPUT_FILENAME "output.txt"
 
 using namespace std;
 
+int numToPositive(int num)
+{
+    return (num < 0) ? num * -1 : num;
+}
+
+const vector<int> SplitToPositiveIntegers(const string &targetString, const char &delimiter)
+{
+    string tmpString("");
+    vector<int> resultVector;
+
+    for (auto prepareString : targetString)
+    {
+        if (prepareString != delimiter)
+        {
+            tmpString += prepareString;
+        }
+        else if (prepareString == delimiter && tmpString != "")
+        {
+            resultVector.push_back(numToPositive(stoi(tmpString)));
+            tmpString = "";
+        }
+    }
+
+    if (tmpString != "")
+    {
+        resultVector.push_back(numToPositive(stoi(tmpString)));
+    }
+    return resultVector;
+}
+
 int main()
 {
-
     ifstream inputFile(INPUT_FILENAME);
     ofstream outputFile(OUTPUT_FILENAME);
 
@@ -19,28 +49,28 @@ int main()
         return 1;
     }
 
-    /*
     string fileLine;
+    int numCount = 0;
     int strCounter = 0;
-    int triangleWeight = 0;
-
-    vector<vector<int>> vectorOfTriangleData;
+    vector<int> vectorOfNums;
 
     while (getline(inputFile, fileLine))
     {
         if (strCounter == 0)
         {
-            triangleWeight = stoi(fileLine);
+            numCount = stoi(fileLine);
+        }
+        else if (strCounter == 1)
+        {
+            vectorOfNums = SplitToPositiveIntegers(fileLine, ' ');
         }
         else
         {
-            vector<int> stringToSplitVector = SplitIntegers(fileLine, ' ');
-            vectorOfTriangleData.push_back(stringToSplitVector);
+            break;
         }
         strCounter++;
     }
 
-    CalculateTriangleMaxSum(triangleWeight, vectorOfTriangleData, outputFile);
-    */
+    cout << accumulate(vectorOfNums.begin(), vectorOfNums.end(), 0) << endl;
     return 0;
 }
